6cabd7281854ded7aad5fef69e4141ab73d52d0b
kev <ktran0125gmail.com>
2018-06-24 00:29:40 -0400

    first commit

--- SearchEngine.py
+++ SearchEngine.py
@@ -2,8 +2,64 @@
 """
 Breakdown:
 1. create function to take in document to be searched and construct word-count dictionary
-
+2. Vector space
 """
 
 
-#Concordance: count of every word that occurs in a document
+
+
+#Vector Space: calculate the distances between two points, infinite planes
+
+
+class VectorCompare:
+    def magnitude(self, concordance): #why first arg self?
+
+        if type(concordance) != dict:
+            raise ValueError("Argument needs to be dictionary.")
+
+        total = 0
+
+        for word, count in concordance.iteritems(): #what is iteritems
+            total += count ** 2
+
+        return math.sqrt(total)
+
+
+    def relation(self, concordance1, concordance2):
+
+        if type(concordance1) != dict:
+            raise ValueError("Arg needs to be a dictionary.")
+
+        if type(concordance2) != dict:
+            raise ValueError("Arg needs to be a dictionary.")
+
+        relevance = 0
+        topvalue = 0
+
+        for word, count in concordance1.iteritems():
+
+            if concordance2.has_key(word):
+                topvalue += count*concordance2[word]
+        if (self.magnitude(concordance1) * self.magnitude(concordance2)) != 0:
+            return topvalue/(self.magnitude(concordance1) * self.magnitude(concordance2))
+        else:
+            return 0
+
+    #Concordance: count of every word that occurs in a document
+
+    def concordance(self, document):
+
+        if type(document) != str:
+            raise ValueError("Argument needs to be a string.")
+
+        conc = {}
+
+        for word in document.split(" "):
+
+            if conc.has_key(word):
+                conc[word] += 1
+
+            else:
+                conc[word] = 1
+
+        return conc
